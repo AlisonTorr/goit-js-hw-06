f6e41fb7d0dcc081e68c780583d1f05f72694433
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-20 15:01:38 +0300

    Add files via upload

--- js/task-6.js
+++ js/task-6.js
@@ -86,8 +86,7 @@ const users = [
 ];
 
 const getUsersWithAge = (users, min, max) => {
-  users.filter((user) => min < user.age || user.age > max);
-  return user;
+  users.filter((user) => min > user.age || user.age < max);
 };
 console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
